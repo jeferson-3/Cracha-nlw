08b45b7450f387531f67267bc781c7cb366d4fc6
jeff batalha estudante <84995245+jeferson-3@users.noreply.github.com>
2021-11-03 21:33:31 -0300

    Update main.js

--- main.js
+++ main.js
@@ -26,7 +26,7 @@ function getGitHubProfileInfos() {
       userBio.textContent = data.bio
       userLink.href = data.html_url
       UserImage.src = data.avatar_url
-      userLink.textContent = data.login
+      userLogin.textContent = data.login
     })
 }
 
